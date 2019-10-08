cae9492a620646cfe4af6ac067bb1311ba4d510f
陈辉 <1273087648@qq.com>
2019-10-08 11:31:34 +0800

    设置静态变量NSDateFormatter，因为NSDateFormatter创建操作比较耗时

--- TableViewPO/TableViewPO/LiveModel.m
+++ TableViewPO/TableViewPO/LiveModel.m
@@ -7,17 +7,20 @@
 //
 
 #import "LiveModel.h"
-
+static NSDateFormatter *formatter;//NSDateFormatter创建操作比较耗时，所以设置静态变量，重复利用
 @implementation LiveModel
+
 - (void)setRoom_name:(NSString *)room_name {
 //    _room_name = [NSString stringWithFormat:@"%@%@%@",room_name,room_name,room_name];
     _room_name = [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@",room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name,room_name];
 }
-//此处的优化：找的接口里面并没有包含时间戳，所以自己造了个。NSDateFormatter时间转换比较耗时，避免在cell赋值时去创建NSDateFormatter。
+//此处的优化：找的接口里面并没有包含时间戳，所以自己造了个。NSDateFormatter时间转换比较耗时，避免在cell赋值时去创建NSDateFormatter。若存在加密数据，转换亦是如此。
 - (void)setRoom_id:(NSString *)room_id {
     NSTimeInterval time = 1569813235 + [room_id doubleValue];
     NSDate *detaildate=[NSDate dateWithTimeIntervalSince1970:time];
-    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    if (!formatter) {
+        formatter = [[NSDateFormatter alloc] init];
+    }
     formatter.dateStyle = NSDateFormatterMediumStyle;
     formatter.timeStyle = NSDateFormatterShortStyle;
     [formatter setDateFormat:@"yyyy-MM-dd HH:mm"];
